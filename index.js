96c3cebbaa88aee74d817d65db85bc23e31c8887
shayla king <shayyylaaaking@gmail.com>
2023-04-02 20:28:07 -0700

    message

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+{let num1 =31;
+let num2 =2;
+let multiply = num1 * num2;
+}
 
-// Write your code here
+{let num3 = 8
+  let num4 = 2
+  let mod = 8/2
+}
+{let random = 10
+Math.floor(Math.random()*10)}
+
+let max = (1, 2, 10, 16, 18, 20); 
